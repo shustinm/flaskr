4b2cac5b2e1ee57efe265e29ca6c5af1d5e3134d
Michael Shustin <michaelshustin@gmail.com>
2018-06-16 15:02:24 +0300

    Added login

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -9,6 +9,19 @@ from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * from user WHERE id = ?', (user_id,)
+        ).fetchone()
+
+
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method is 'POST':
@@ -38,6 +51,7 @@ def register():
 
     return render_template('auth/register.html')
 
+
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method is 'POST':
@@ -63,3 +77,19 @@ def login():
 
     return render_template('auth/login.html')
 
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
